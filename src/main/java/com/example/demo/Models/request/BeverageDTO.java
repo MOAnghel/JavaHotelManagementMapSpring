277cf07c6204edcedcf08dbce1035407b135d15c
Robert <robert.burlacu03@gmail.com>
2024-01-11 22:20:17 +0200

    beverage updates

--- src/main/java/com/example/demo/Models/request/BeverageDTO.java
+++ src/main/java/com/example/demo/Models/request/BeverageDTO.java
@@ -1,5 +1,14 @@
 package com.example.demo.Models.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class BeverageDTO {
     private Integer volume;
 
